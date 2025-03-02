f0019cb88c725128a3dd99522922ef171243c497
Ryan Fitzgerald <fitz297@gmail.com>
2025-03-01 21:48:26 -0500

    Initial

---
+++ chapter_01.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "chapters.h"
+void chapter_01() {
+    printf("Hello World\n");
+}
---
+++ chapters.h
@@ -0,0 +1,10 @@
+//
+// Created by Ryan Fitzgerald on 3/1/25.
+//
+
+#ifndef CHAPTERS_H
+#define CHAPTERS_H
+
+void chapter_01();
+
+#endif //CHAPTERS_H
